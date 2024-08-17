edfb4e983967f5381b995f0138de49504837f203
Stephen Man <stephenman88@gmail.com>
2024-08-17 00:22:07 -0400

    implemented compatibility with shout and silvie.org formats

--- src/component/OmniForm/OmniForm.js
+++ src/component/OmniForm/OmniForm.js
@@ -6,8 +6,40 @@ function OmniForm({onSubmit, onCancel}){
     const mainDeck = useRef()
     const sideDeck = useRef()
 
-    function onPaste(e){
+    const [matDeckState, setMatDeckState] = useState("")
+    const [mainDeckState, setMainDeckState] = useState("")
+    const [sideDeckState, setSideDeckState] = useState("")
 
+    function onPaste(e){
+        let validDeckPasted = false
+        const clipboardText = e.clipboardData.getData('text/plain')
+        const clipboardList = clipboardText.split('#')
+        clipboardList.forEach(segment => {
+            if(segment.startsWith(' Material Deck\r')){
+                const segmentArray = segment.split('\n')
+                let filteredArray = segmentArray.filter((line) => {return line.match('^[0-9]')})
+                let deckList = filteredArray.join('\n')
+                setMatDeckState(deckList)
+                validDeckPasted = true;
+            }
+            if(segment.startsWith(' Main Deck\r')){
+                const segmentArray = segment.split('\n')
+                let filteredArray = segmentArray.filter((line) => {return line.match('^[0-9]')})
+                let deckList = filteredArray.join('\n')
+                setMainDeckState(deckList)
+                validDeckPasted = true;
+            }
+            if(segment.startsWith(' Sideboard\r')){
+                const segmentArray = segment.split('\n')
+                let filteredArray = segmentArray.filter((line) => {return line.match('^[0-9]')})
+                let deckList = filteredArray.join('\n')
+                setSideDeckState(deckList)
+                validDeckPasted = true;
+            }
+        });
+        if (validDeckPasted){
+            e.preventDefault()
+        }
     }
 
     return(
@@ -17,15 +49,15 @@ function OmniForm({onSubmit, onCancel}){
                 <div className='omniform-form-inputarea'>
                     <div className='omniform-form-inputarea-inputset'>
                         <label className="omniform-form-inputarea-inputset_label" htmlFor='omniform-mat-deck'>Material Deck</label>
-                        <textarea className="omniform-form-inputarea-inputset_textarea" ref={matDeck} onPaste={(e)=>{onPaste(e)}} rows='7' id='omniform-mat-deck' name="mat_deck"></textarea>  
+                        <textarea className="omniform-form-inputarea-inputset_textarea" onChange={(e)=>{setMatDeckState(e.target.value)}} ref={matDeck} onPaste={(e)=>{onPaste(e)}} rows='7' id='omniform-mat-deck' name="mat_deck" value={matDeckState}></textarea>  
                     </div>
                     <div className='omniform-form-inputarea-inputset'>
                         <label className="omniform-form-inputarea-inputset_label" htmlFor='omniform-main-deck'>Main Deck</label>
-                        <textarea className="omniform-form-inputarea-inputset_textarea" ref={mainDeck} onPaste={(e)=>{onPaste(e)}} rows='7' id='omniform-main-deck' name="main_deck"></textarea>
+                        <textarea className="omniform-form-inputarea-inputset_textarea" onChange={(e)=>{setMainDeckState(e.target.value)}} ref={mainDeck} onPaste={(e)=>{onPaste(e)}} rows='7' id='omniform-main-deck' name="main_deck" value={mainDeckState}></textarea>
                     </div>
                     <div className='omniform-form-inputarea-inputset'>
-                        <label className="omniform-form-inputarea-inputset_label" htmlFor='omniform-side-deck'>Side Deck</label>
-                        <textarea className="omniform-form-inputarea-inputset_textarea" ref={sideDeck} onPaste={(e)=>{onPaste(e)}} rows='7' id='omniform-side-deck' name="side_deck"></textarea>
+                        <label className="omniform-form-inputarea-inputset_label" htmlFor='omniform-side-deck'>Sideboard</label>
+                        <textarea className="omniform-form-inputarea-inputset_textarea" onChange={(e)=>{setSideDeckState(e.target.value)}} ref={sideDeck} onPaste={(e)=>{onPaste(e)}} rows='7' id='omniform-side-deck' name="side_deck" value={sideDeckState}></textarea>
                     </div>
                 </div>
                 <div className='omniform-form-buttons'>
